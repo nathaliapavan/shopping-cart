f83ed4db682fa9d6212b7e7759dbb2cd592a7efc
Nathalia Pavan <nanahpavan@gmail.com>
2022-07-13 19:37:17 -0300

    feat: initial commit

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Nathalia Pavan',
+            'email' => 'teste@teste.com',
+            'password' => Hash::make('123'),
+        ]);
+    }
+}
+
+// php artisan db:seed
